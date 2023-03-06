a0fcd027707d3a4860873824c6d173c4d0e2d04b
AgoraNgelale <agorangelale@icloud.com>
2023-03-06 02:40:16 -0800

    setting up gtest

---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+TEST(ConstructorTests, TestArgs) {
+  
+    EXPECT_EQ(3, 3);
+}
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
